61b4c13bb1e07f31c59eaa271cee60d7d56f4f5f
Marco0622 <slendsher48@gmail.com>
2026-04-15 16:28:57 +0200

    Gestion de grade et l'exclusion

--- src/Controller/AccessController.php
+++ src/Controller/AccessController.php
@@ -13,52 +13,70 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-/**
- * @todo sécuriser la création des accés avec un voter 
- */
-
 #[Route('/access', name: 'app_access_')]
 final class AccessController extends AbstractController
 {
 
 
-    #[Route('/add/{id<\d+>}', name: 'add', methods: ['GET'])]
-    #[IsGranted('IS_RECEIVER', subject: 'session', message: "Vous devez avoir était invité !")]
-    public function add(Session $session, EntityManagerInterface $entityManager, AccessRepository $accessRepository): Response
-    {
-       
-        $objAccess = new Access();
+   #[Route('/add/{id<\d+>}', name: 'add', methods: ['GET'])]
+   #[IsGranted('IS_RECEIVER', subject: 'session', message: "Vous devez avoir était invité !")]
+   public function add(Session $session, EntityManagerInterface $entityManager, AccessRepository $accessRepository): Response
+   {
 
-        $objAccess->setJoinedAt(new DateTimeImmutable('now'));
-        $objAccess->setRole('ROLE_OWNER');
-        $objAccess->setMember($this->getUser());
-        $objAccess->setSession($session);
+      $objAccess = new Access();
 
-        $entityManager->persist($objAccess);
-        $entityManager->flush();
+      $objAccess->setJoinedAt(new DateTimeImmutable('now'));
+      $objAccess->setRole('ROLE_OWNER');
+      $objAccess->setMember($this->getUser());
+      $objAccess->setSession($session);
 
-        $this->addFlash('success', 'Bienvenu dans la session : '. $session->getName());
+      $entityManager->persist($objAccess);
+      $entityManager->flush();
 
-       return $this->redirectToRoute('app_session_home', [
-            'id' => $session->getId(),
-       ]);
-    }
+      $this->addFlash('success', 'Bienvenu dans la session : ' . $session->getName());
+
+      return $this->redirectToRoute('app_session_home', [
+         'id' => $session->getId(),
+      ]);
+   }
 
 
-    #[Route('/remove/{id<\d+>}', name: 'remove', methods: ['POST'])]
-    #[IsGranted('IS_OWNER', subject: 'session', message: "Droit insuffisant  Seul le propriétaire de la session peux exclure un utilisateur !")]
-    public function remove(Session $session, EntityManagerInterface $entityManager, AccessRepository $accessRepository): Response
-    {
-        
+   #[Route('/remove/{id<\d+>}', name: 'remove', methods: ['POST'])]
+   #[IsGranted('IS_OWNER', subject: 'session', message: "Droit insuffisant  Seul le propriétaire de la session peux exclure un utilisateur !")]
+   public function remove(Session $session, EntityManagerInterface $entityManager, AccessRepository $accessRepository, Request $request): Response
+   {
+      if (!$this->isCsrfTokenValid('remove_access', $request->request->get('_token'))) {
+         throw $this->createAccessDeniedException('Token CSRF invalide.');
+      }
 
-       return $this->redirectToRoute('app_user_home');
-    }
+      $idUserToRemove = $request->request->get('userToRemove');
 
-     
-    #[Route('/leave/{id<\d+>}', name: 'leave', methods: ['POST'])]
-    #[IsGranted('REMOVE_ACCESS', subject: 'session', message: "Vous devez étre menbre de la session et ne pas étre le propriétaire !")]
-    public function leave(Session $session, EntityManagerInterface $entityManager, AccessRepository $accessRepository, Request $request): Response
-    {
+      $objUserToRemove = $accessRepository->findOneBy([
+         'session' => $session,
+         'member' => $idUserToRemove,
+      ]);
+
+      if (is_null($objUserToRemove) || $this->getUser() == $objUserToRemove) {
+         $this->addFlash('warning', 'Une erreur est survenue !');
+         return $this->redirectToRoute('app_user_home');
+      }
+
+      $entityManager->remove($objUserToRemove);
+      $entityManager->flush();
+
+      $this->addFlash('success', "L'utilisateur a été exclu !");
+
+
+      return $this->redirectToRoute('app_session_home', [
+         'id' => $session->getId(),
+      ]);
+   }
+
+
+   #[Route('/leave/{id<\d+>}', name: 'leave', methods: ['POST'])]
+   #[IsGranted('REMOVE_ACCESS', subject: 'session', message: "Vous devez étre menbre de la session et ne pas étre le propriétaire !")]
+   public function leave(Session $session, EntityManagerInterface $entityManager, AccessRepository $accessRepository, Request $request): Response
+   {
       if (!$this->isCsrfTokenValid('leave_session', $request->request->get('inv_token'))) {
          throw $this->createAccessDeniedException('Token CSRF invalide.');
       }
@@ -68,9 +86,11 @@ final class AccessController extends AbstractController
          'member' => $this->getUser(),
       ]);
 
-      if(is_null($objUserAccess)){
+      if (is_null($objUserAccess)) {
          $this->addFlash('warning', 'Une erreur est survenue !');
-         return $this->redirectToRoute('app_user_home');
+         return $this->redirectToRoute('app_session_home', [
+            'id' => $session->getId(),
+         ]);
       }
 
       $entityManager->remove($objUserAccess);
@@ -79,16 +99,48 @@ final class AccessController extends AbstractController
       $this->addFlash('success', "Vous avez quitté la session !");
 
 
-       return $this->redirectToRoute('app_user_home');
-    }
+      return $this->redirectToRoute('app_user_home');
+   }
 
 
-    #[Route('/role/{id<\d+>}', name: 'role', methods: ['POST'])]
-    #[IsGranted('IS_OWNER', subject: 'session', message: "Droit insuffisant Seul le propriétaire de la session peux exclure un utilisateur !")]
-    public function role(Session $session, EntityManagerInterface $entityManager, AccessRepository $accessRepository): Response
-    {
-        
+   #[Route('/role/{id<\d+>}', name: 'role', methods: ['POST'])]
+   #[IsGranted('IS_OWNER', subject: 'session', message: "Droit insuffisant Seul le propriétaire de la session peux exclure un utilisateur !")]
+   public function role(Session $session, EntityManagerInterface $entityManager, AccessRepository $accessRepository, Request $request): Response
+   {
+      if (!$this->isCsrfTokenValid('role_access', $request->request->get('_token'))) {
+         throw $this->createAccessDeniedException('Token CSRF invalide.');
+      }
 
-       return $this->redirectToRoute('app_user_home');
-    }
+      $idUserToUpdate = $request->request->get('userToUpdate');
+      $role = $request->request->get('role');
+
+      $objUserToUpdate = $accessRepository->findOneBy([
+         'session' => $session,
+         'member' => $idUserToUpdate,
+      ]);
+
+      if (is_null($idUserToUpdate) || $this->getUser() == $idUserToUpdate || is_null($role)) {
+         $this->addFlash('warning', 'Une erreur est survenue !');
+         return $this->redirectToRoute('app_session_home', [
+            'id' => $session->getId(),
+         ]);
+      }
+
+      if($role === 'visitor'){
+         $objUserToUpdate->setRole('ROLE_VISITOR');
+      }  
+      if($role === 'editor'){
+         $objUserToUpdate->setRole('ROLE_EDITOR');
+      }
+
+      $entityManager->flush();
+
+      $this->addFlash('success', "Le rôle a été modifié !");
+
+
+      return $this->redirectToRoute('app_session_home', [
+         'id' => $session->getId(),
+      ]);
+
+   }
 }
